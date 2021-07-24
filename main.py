2d108cd9bd5e1b3535b696ad0dec7446fa34542a
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-24 17:58:39 -0500

    Conditional for rendering score to game over screen + styling.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def display_score():
     score_surf = test_font.render(f"Score: {current_time}", False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf, score_rect)
+    return current_time
 
 # # SETUP BASIC VARIABLES - - - - - - - - - - - - - - - - - - -
 pygame.init() # <-- initialize pygame 
@@ -15,8 +16,9 @@ screen = pygame.display.set_mode((800,400)) # <-- declare screen variable, set w
 pygame.display.set_caption("Terry's Game of Life") # <-- changes window title
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 # # SURFACES & RECTANGLES - - - - - - - - - - - - - - - - - - -
 sky_surf = pygame.image.load("graphics/Sky.png").convert()
@@ -28,6 +30,17 @@ snail_rect = snail_surf.get_rect(midbottom = (600,300))
 player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha() # <-- interactable objects always require both a surface and rectangle, or a sprite
 player_rect = player_surf.get_rect(midbottom = (80,300)) # <-- creates rectangle around a given surface beginning from a given origin
 player_gravity = 0
+
+# INTRO SCREEN - - - - - - - - - - - - - - - - - - -
+player_stand = pygame.image.load("graphics/player/player_stand.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2) # <-- rotozoom(variable to be zoomed, angle, scale)
+player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render("Pixel Runner", False, (111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render("Press SPACE to run", False, (111,196,169))
+game_message_rect = game_message.get_rect(center = (400, 330))
 
 # # GAME LOOP - - - - - - - - - - - - - - - - - - -
 while True: # <-- runs forever, renders game, until player input sets False
@@ -53,10 +66,7 @@ while True: # <-- runs forever, renders game, until player input sets False
         # # POST-EVENT RENDERING - - - - - - - - - - - - - - - - - - -
         screen.blit(sky_surf, (0,0))
         screen.blit(ground_surf, (0,300))
-        # pygame.draw.rect(screen, (195,234,233), score_rect)
-        # pygame.draw.rect(screen, (195,234,233), score_rect, 6) # <-- surface, color, which rectangle, width
-        # screen.blit(score_surf, score_rect) # <-- block image transfer: places surface on screen
-        display_score()
+        score = display_score()
 
         # # SNAIL MOVEMENT - - - - - - - - - - - - - - - - - - -
         snail_rect.left -= 6
@@ -77,7 +87,16 @@ while True: # <-- runs forever, renders game, until player input sets False
 
     # # RENDER GAME-OVER STATE  - - - - - - - - - - - - - - - - - - -
     else:
-        screen.fill("Yellow")
+        screen.fill((94,129,162))
+        screen.blit(player_stand, player_stand_rect)
+        score_message = test_font.render(f"Your Score: {score}", False, (111,196,169))
+        score_message_rect = score_message.get_rect(center = (400,330))
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
 
     # # UPDATE CLOCK AND DISPLAY - - - - - - - - - - - - - - - - - - -
     pygame.display.update() # <-- update the screen display
@@ -114,3 +133,6 @@ while True: # <-- runs forever, renders game, until player input sets False
         # # SAMPLE SCORE TEXT
         # score_surf = test_font.render("My Game", False, (64,64,64)) # <-- renders font with text, anti-aliasing Boolean, and color
         # score_rect = score_surf.get_rect(center = (400,50))
+        # pygame.draw.rect(screen, (195,234,233), score_rect)
+        # pygame.draw.rect(screen, (195,234,233), score_rect, 6) # <-- surface, color, which rectangle, width
+        # screen.blit(score_surf, score_rect) # <-- block image transfer: places surface on screen
